c52215248a9d4a47a90fcaf2dc843832f1d3d0e5
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-13 13:47:17 -0500

    WIP: Created TodoReducer.js in reducers folder.

---
+++ todo/src/reducers/TodoReducer.js
@@ -0,0 +1,63 @@
+import React, { useReducer } from 'react';
+
+export const intitailState = { 
+    todoList: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+        },
+        
+        {
+            item: 'Study Chinese',
+            completed: false,
+            id: 389298758
+        }, 
+
+        {
+            item: 'Look for a new backpack',
+            completed: false,
+            id: 3892987589
+        }, 
+
+    ]
+};
+
+
+export const TodoReducer = (state, action) => {
+    switch(action.type) {
+        case "ADD_TODO":
+            return {
+                ...state, 
+                todoList: [
+                    ...todoList, 
+                    {
+                        item: action.item,
+                        completed: false, 
+                        id: Date.now()
+                    }
+                ]
+            }
+            
+        case "REMOVE_TODO":
+                return {
+                    ...state,
+                    todoList: state.todoList.filter(todoObject => todoObject.id !== action.id)
+                }
+
+        case "TOGGLE_COMPLETED":
+            return {
+                ...state,
+                todoList: state.todoList.map(todoObject => {
+                    if (todoObject.id === action.id) {
+                      return {
+                          ...todoObject, 
+                          completed: !todoObject.completed
+                      }
+                    }
+                    return todoObject   
+                })
+            }
+    }
+   
+};
